80363f1d4a4d95afa1c3fdd6ce213b5da58a445f
atommnava <atommunoznava@gmail.com>
2026-01-29 22:04:28 -0600

    Init commit

---
+++ src/main/java/com/informaticonfig/spring/apprutas/springboot_apprutas/SpringbootApprutasApplication.java
@@ -0,0 +1,13 @@
+package com.informaticonfig.spring.apprutas.springboot_apprutas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApprutasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApprutasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informaticonfig/spring/apprutas/springboot_apprutas/SpringbootApprutasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informaticonfig.spring.apprutas.springboot_apprutas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApprutasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
